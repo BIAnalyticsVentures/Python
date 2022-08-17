49fedc0da8cd426939d103fc98212f29820aa4fb
BIAnalyticsVentures <100650972+BIAnalyticsVentures@users.noreply.github.com>
2022-08-17 20:17:21 +1000

    Create Cancer.py

---
+++ HealthCare/Disease Cancer/Cancer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Machine Learning on Breast Cancer
+Created on Sun Mar 21 21:55:00 2021
+@Author:  @BIVentures of @BIAnalyticsVentures
+"""
+
+#importing the libraries
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+pd.set_option('display.max_columns', 100)
+
+os.chdir('C:/DirectoryPathHere/')
+
+#importing our cancer dataset
+df = pd.read_csv('cancer.csv',index_col= None, na_values='?')
+
+#Get X and Y
+X = df.iloc[:,1:31].values
+Y = df.iloc[:,:31].values
+
+#print top 5 rows
+print(df.head())
+print('\n')
