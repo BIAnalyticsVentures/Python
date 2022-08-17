613d40c0defefde9fad41f6350ffe6c1f58ece00
BIAnalyticsVentures <100650972+BIAnalyticsVentures@users.noreply.github.com>
2022-08-17 18:38:13 +1000

    Create CreditCard.py
    Financial Credit Card

---
+++ Financial/Credit Card/CreditCard.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 30 12:23:55 2020
+@Author: BIVentures of BIAnalyticsVentures
+"""
+
+#Choropleth maps are popular thematic maps to represent statistical data on predetermined geographic areas (i.e., countries)
+#Choropleth maps are used through various shading patterns/symbols in relation to a data variable  
+#Choropleth maps are good at utilizing data to easily represent the variability of the desired measurement across a region
+
+import pickle
+import os.path
+import time
+import warnings
+warnings.filterwarnings('ignore')
+
+##Load Libraries
+##for conda-
+#conda install plotly
+#conda install -c conda-forge cufflinks-py
+
+import plotly
+import plotly.graph_objs as go 
+from plotly.offline import init_notebook_mode,iplot,plot
+init_notebook_mode(connected=True)
+
+import pandas as pd
+
+##Get & Set current working directory path
+path = os.getcwd()
+#print(path)
+#print('\n')
+
+filepath=os.chdir('C:/Users/windo/Desktop/TextMiningR/PythonScripts')
+print(filepath)
+#print('\n')
